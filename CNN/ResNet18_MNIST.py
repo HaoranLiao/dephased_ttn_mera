8a996c6559380f6cdd0ecf6bea13c282822b3d09
Zhibo Yang <zyangaj@gmail.com>
2021-12-17 18:45:12 -0500

    restructured

--- CNN/ResNet18_MNIST.py
+++ CNN/ResNet18_MNIST.py
@@ -18,7 +18,7 @@ import torch.nn as nn
 import sys
 # insert at 1, 0 is the script path (or '' in REPL)
 sys.path.insert(1, './uni_ttn/tf2.7/')
-import data as datagen
+import data
 
 # n_epochs = 3
 # batch_size_train = 50
@@ -31,59 +31,56 @@ import data as datagen
 # torch.backends.cudnn.enabled = False
 # torch.manual_seed(random_seed)
 
-def load_train_images(sample_size=5000):
-	(train_data, val_data, test_data) = datagen.get_data_web([3,5], 0, [8,8], 2, sample_size=sample_size)
+def load_data(digits, sample_size, val_split=0):
+	datagen = data.DataGenerator()
+	datagen.shrink_images([8, 8])
+
+	(train_images, train_labels), _, (test_images, test_labels) = data.process(
+		datagen.train_images, datagen.train_labels, 
+		datagen.test_images, datagen.test_labels,
+		 digits, val_split, sample_size=sample_size 
+	)
 				
-	(train_images, train_labels) = train_data
-	train_images = np.reshape(train_images, [sample_size, 1, 8, 8])
-	train_images = torch.from_numpy(train_images).to(dtype=torch.float32, )
-	train_labels = torch.from_numpy(train_labels).to(dtype=torch.int64)
+	train_images = np.reshape(train_images, [-1, 1, 8, 8])
+	train_images = torch.from_numpy(train_images).to(dtype=torch.float32)
+	train_labels = torch.from_numpy(train_labels).to(dtype=torch.float32)
 		 
-	(test_images, test_labels) = test_data
-	test_images = np.reshape(test_images, [sample_size, 1, 8, 8])
+	test_images = np.reshape(test_images, [-1, 1, 8, 8])
 	test_images = torch.from_numpy(test_images).to(dtype=torch.float32)
-	test_labels = torch.from_numpy(test_labels).to(dtype=torch.int64)
-	
+	test_labels = torch.from_numpy(test_labels).to(dtype=torch.float32)
+
 	return (train_images, train_labels, test_images, test_labels)
 		
 class resnet18_mod(models.resnet.ResNet):
 #Modifying resnet18 for 1-channel grayscale MNIST
 #https://discuss.pytorch.org/t/modify-resnet-or-vgg-for-single-channel-grayscale/22762
 	def __init__(self, block, layers, num_classes=2):
-		self.inplanes = 64
 		super(resnet18_mod, self).__init__(block, layers, num_classes)
-		self.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3,
-							   bias=False)
+		self.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+
+		self.loss_func = nn.CrossEntropyLoss()
+		self.optimizer = optim.Adam(self.parameters())
 
 
-def train(network, optimizer, train_images, train_labels):
-	network.train()
-	batch_idx = 0
-	batch_iter_train = datagen.batch_generator(train_images, train_labels, 100)
-	for (data, target) in batch_iter_train:
-		batch_idx += 1
-		optimizer.zero_grad()
-		output = network(data)
-		# loss_func = nn.CrossEntropyLoss()
-		# loss = loss_func(output,target)
-		loss = ((output - target)**2).mean()
-		loss.backward()
-		optimizer.step()
-	  
-def test(network, test_images, test_labels):
-	network.eval()
-	test_loss = 0
-	correct = 0
-	with torch.no_grad():
-		batch_iter_test = datagen.batch_generator(test_images, test_labels, 1000)
-		for data, target in batch_iter_test:
-			output = network(data)
-			# loss_func = nn.CrossEntropyLoss()
-			# test_loss += loss_func(output,target)
-			test_loss += ((output - target)**2).mean()
-			correct += get_accuracy(output, target)
+	def train(self, train_images, train_labels, batch_size):
+		batch_iter_train = data.batch_generator_np(train_images, train_labels, batch_size)
+		for images, labels in batch_iter_train:
+			self.optimizer.zero_grad()
+			pred_labels = self(images)
+			loss = self.loss_func(pred_labels, labels)
+			loss.backward()
+			self.optimizer.step()
+		
+	def test(self, test_images, test_labels, batch_size):
+		correct = 0
+		with torch.no_grad():
+			batch_iter_test = data.batch_generator_np(test_images, test_labels, batch_size)
+			for images, labels in batch_iter_test:
+				pred_labels = self(images)
+				correct += get_accuracy(pred_labels, labels)[1]
 
-	return correct
+		test_accuracy = correct / test_images.shape[0]
+		return test_accuracy
 
 def get_accuracy(output, target):
 	output_index = np.argmax(output, axis=1)
@@ -93,18 +90,21 @@ def get_accuracy(output, target):
 	num_correct = float(np.sum(compare == 0))
 	total = float(output_index.shape[0])
 	accuracy = num_correct / total
-	return accuracy
+	return accuracy, num_correct
 
 def main():
-	(train_images, train_labels, test_images, test_labels) = load_train_images()
-	network = resnet18_mod(block = models.resnet.Bottleneck, layers = [2, 2, 2, 2] )
-	optimizer = optim.Adam(network.parameters())
+	digits = [3, 5]
+	sample_size = 5000
+	(train_images, train_labels, test_images, test_labels) = load_data(digits, sample_size)
+	network = resnet18_mod(block=models.resnet.Bottleneck, layers=[2, 2, 2, 2])
+	num_epochs = 100
+	train_batch_size = 250
 	
-	print('Test Accuracy: %.3f'%test(network, test_images, test_labels))
-	for epoch in range(1, 101):
+	for epoch in range(num_epochs):
 		print('Epoch: %s'%epoch)
-		train(network, optimizer, train_images, train_labels)
-		print('Test Accuracy: %.3f'%test(network, test_images, test_labels))
+		network.train(train_images, train_labels, train_batch_size)
+		test_accuracy = network.test(test_images, test_labels, 1000000)
+		print('Test Accuracy: %.3f'%test_accuracy)
 	
 	torch.save(network.state_dict(), '../trained_models/samp1000_size8.pth')
 	print('Model saved')
