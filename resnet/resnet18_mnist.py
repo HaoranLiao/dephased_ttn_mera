0de99da55b222af4179f3de6bdccde7e116376b7
Haoran Liao <haoran.liao@berkeley.edu>
2022-02-16 22:49:42 -0800

    update to be compatible with torch 1.7

--- resnet/resnet18_mnist.py
+++ resnet/resnet18_mnist.py
@@ -15,8 +15,7 @@ import torch.optim as optim
 import numpy as np
 import torch.nn as nn
 import sys
-# insert at 1, 0 is the script path (or '' in REPL)
-sys.path.insert(1, 'dephased_ttn_project/uni_ttn/tf2.7/')
+sys.path.append('../uni_ttn/tf2.7/')
 import data
 
 
@@ -33,11 +32,11 @@ def load_data(digits, sample_size):
 				
 	train_images = np.reshape(train_images, [-1, 1, 8, 8])
 	train_images = torch.from_numpy(train_images).to(dtype=torch.float32, device=device)
-	train_labels = torch.from_numpy(train_labels).to(dtype=torch.float32, device=device)
+	train_labels = torch.from_numpy(np.argmax(train_labels, axis=1)).to(dtype=torch.long, device=device)
 
 	test_images = np.reshape(test_images, [-1, 1, 8, 8])
 	test_images = torch.from_numpy(test_images).to(dtype=torch.float32, device=device)
-	test_labels = torch.from_numpy(test_labels).to(dtype=torch.float32, device=device)
+	test_labels = torch.from_numpy(np.argmax(test_labels, axis=1)).to(dtype=torch.long, device=device)
 
 	return train_images, train_labels, test_images, test_labels
 
@@ -54,7 +53,7 @@ class resnet18_mod(models.resnet.ResNet):
 		self.loss_func = nn.CrossEntropyLoss()
 		self.optimizer = optim.Adam(self.parameters())
 
-	def train(self, train_images, train_labels, batch_size):
+	def train_network(self, train_images, train_labels, batch_size):
 		batch_iter = data.batch_generator_np(train_images, train_labels, batch_size)
 		for images, labels in batch_iter:
 			self.optimizer.zero_grad()
@@ -71,20 +70,17 @@ class resnet18_mod(models.resnet.ResNet):
 			batch_iter = data.batch_generator_np(images, labels, batch_size)
 			for image_batch, label_batch in batch_iter:
 				pred_labels = self(image_batch)
-				num_correct += get_accuracy(pred_labels, label_batch)[1]
+				num_correct += get_accuracy(pred_labels, label_batch)
 
 		accuracy = num_correct / len(images)
 		return accuracy
 
-def get_accuracy(output, target):
+def get_accuracy(output, target_index):
 	output_index = np.argmax(output, axis=1)
-	target_index = np.argmax(target, axis=1)
 	compare = output_index - target_index
 	compare = compare.numpy()
 	num_correct = float(np.sum(compare == 0))
-	total = float(output_index.shape[0])
-	accuracy = num_correct / total
-	return accuracy, num_correct
+	return num_correct
 
 def main():
 	digits = [3, 5]
@@ -96,13 +92,13 @@ def main():
 	network = resnet18_mod(block=models.resnet.Bottleneck, layers=[2, 2, 2, 2])
 	
 	for epoch in range(num_epochs):
-		training_accuracy = network.train(train_images, train_labels, train_batch_size)
+		training_accuracy = network.train_network(train_images, train_labels, train_batch_size)
 		print(f'Epoch: {epoch}: {training_accuracy:.3f}')
 		if not epoch%5:
 			test_accuracy = network.run_network(test_images, test_labels)
 			print('Test Accuracy: %.3f'%test_accuracy)
 	
-	torch.save(network.state_dict(), './trained_models/samp1000_size8.pth')
+	torch.save(network.state_dict(), './trained_models/samp5000_size8.pth')
 	print('Model saved')
 
 
