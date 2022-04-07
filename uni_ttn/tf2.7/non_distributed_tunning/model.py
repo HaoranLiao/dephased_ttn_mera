adcb59194f874f0e314252e25d39d1bc277c990e
Haoran Liao <haoran.liao@berkeley.edu>
2022-04-06 19:36:22 -0700

    update

--- uni_ttn/tf2.7/non_distributed_tunning/model.py
+++ uni_ttn/tf2.7/non_distributed_tunning/model.py
@@ -250,8 +250,8 @@ if __name__ == "__main__":
 
     asha_scheduler = tune.schedulers.ASHAScheduler(
         time_attr='training_iteration',
-        max_t=200,
-        grace_period=50
+        max_t=100,
+        grace_period=40
     )
 
     ax_search = AxSearch(metric="score")
@@ -265,22 +265,23 @@ if __name__ == "__main__":
         metric='test_accuracy',
         mode='max',
         verbose=3,
-        num_samples=60,
+        num_samples=1,
         config={'num_anc': num_anc,
                 'deph_p': deph_p,
-                'tune_lr': 0, # not used in spsa
-                'tune_init_std': 0.1,
-                'a': tune.uniform(1, 50),
-                'b': tune.uniform(1, 50),
-                'A': tune.uniform(1, 10),
-                's': tune.uniform(0, 5),
-                't': tune.uniform(0, 3),
-                'gamma': tune.uniform(0, 1)},
+                'tune_lr': tune.grid_search([0.005, 0.025, 0.125]) #0, # not used in spsa
+                'tune_init_std': tune.grid_search([10, 1, 0.1, 0.01, 0.001]) #0.1,
+                # 'a': tune.uniform(1, 50),
+                # 'b': tune.uniform(1, 50),
+                # 'A': tune.uniform(1, 10),
+                # 's': tune.uniform(0, 5),
+                # 't': tune.uniform(0, 3),
+                # 'gamma': tune.uniform(0, 1)
+                },
         local_dir='~/dephased_ttn_project/uni_ttn/ray_results/angle_spsa/',
         resources_per_trial={'cpu': 12, 'gpu': 1},
         scheduler=asha_scheduler,
         progress_reporter=tune.CLIReporter(max_progress_rows=100),
-        search_alg=ax_search,
+        # search_alg=ax_search,
         log_to_file=True,
         name='anc%.0f_deph%.0f' % (num_anc, deph_p)
     )
