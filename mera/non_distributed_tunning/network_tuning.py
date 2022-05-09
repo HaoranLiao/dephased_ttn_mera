b30b7a3172899841c6cad85e5489bd1cc9f9e58b
Haoran Liao <haoran@physics.mcgill.ca>
2022-05-08 20:26:43 -0700

    Update network_tuning.py

--- mera/non_distributed_tunning/network_tuning.py
+++ mera/non_distributed_tunning/network_tuning.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import numpy as np
 from uni_ttn.tf2 import spsa
-from mera import network, network_ancilla
+from mera import network, network_ancilla, network_8inputs
 
 class Tuning_Network(network.Network):
     def __init__(self, num_pixels, deph_p, num_anc, config, tune_config):
@@ -47,3 +47,25 @@ class Tuning_Network_Ancilla(network_ancilla.Network):
             loss = self.loss(input_batch, label_batch)
         grads = tape.gradient(loss, self.var_list)
         self.opt.apply_gradients(zip(grads, self.var_list))
+
+class Tuning_Network_8inputs(network_8inputs.Network):
+        def __init__(self, num_pixels, deph_p, num_anc, config, tune_config):
+            super().__init__(num_pixels, deph_p, num_anc, -1, -1, config)
+
+            self.init_std = tune_config['tune_init_std']
+
+            if config['tree']['opt']['opt'] == 'adam':
+                if not tune_config.get('tune_lr', False): self.opt = tf.keras.optimizers.Adam()
+                else: self.opt = tf.keras.optimizers.Adam(tune_config['tune_lr'])
+            elif config['tree']['opt']['opt'] == 'spsa':
+                self.opt = spsa.Spsa(self, tune_config)
+            else:
+                raise NotImplementedError
+
+        def update_no_processing(self, input_batch: np.ndarray, label_batch: np.ndarray):
+            input_batch = tf.constant(input_batch, dtype=tf.complex64)
+            label_batch = tf.constant(label_batch, dtype=tf.float32)
+            with tf.GradientTape() as tape:
+                loss = self.loss(input_batch, label_batch)
+            grads = tape.gradient(loss, self.var_list)
+            self.opt.apply_gradients(zip(grads, self.var_list))
