91d639cad3edf64db22d4c2c40e9bcd9ec88a9c5
Haoran Liao <haoran@physics.mcgill.ca>
2022-05-09 20:41:50 -0700

    archiv

--- uni_ttn/tf2/archiv/non_identity_initialization/model.py
+++ uni_ttn/tf2/archiv/non_identity_initialization/model.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 import numpy as np
-import sys, os, time, yaml, json
+import sys, time, yaml, json
 from tqdm import tqdm
 import network
-sys.path.append('../')
+sys.path.append('../../')
 import data
 #os.environ["CUDA_VISIBLE_DEVICES"] = '1'
 TQDM_DISABLED = False
