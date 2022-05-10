37fa851ccc9938b9487c1098d637b9592491a268
Haoran Liao <haoran@physics.mcgill.ca>
2022-05-09 20:31:51 -0700

    Update model_tuning.py

--- uni_ttn/tf2/non_distributed_tuning/model_tuning.py
+++ uni_ttn/tf2/non_distributed_tuning/model_tuning.py
@@ -16,7 +16,7 @@ TQDM_DISABLED = True
 TQDM_DICT = {'leave': False, 'disable': TQDM_DISABLED, 'position': 0}
 
 
-class Model(model.Model):
+class Tuning_Model(model.Model):
     def __init__(self, data_path, digits, val_split, deph_p, num_anc, config, tune_config):
         super().__init__(data_path, digits, val_split, deph_p, num_anc, tune_config['tune_init_std'], tune_config['tune_lr'], config)
 
@@ -58,7 +58,7 @@ class UniTTN(tune.Trainable):
         digits = variable_or_uniform(list_digits, 0)
         deph_p = variable_or_uniform(list_deph_p, 0)
         num_anc = variable_or_uniform(list_num_anc, 0)
-        self.model = Model(data_path, digits, val_split, deph_p, num_anc, config, tune_config)
+        self.model = Tuning_Model(data_path, digits, val_split, deph_p, num_anc, config, tune_config)
 
     def step(self):
         self.model.run_epoch(batch_size, self.iteration, grad_accumulation=config['data']['grad_accumulation'])
@@ -67,7 +67,7 @@ class UniTTN(tune.Trainable):
 
 
 if __name__ == "__main__":
-    with open('config_example.yaml', 'r') as f:
+    with open('./config_example.yaml', 'r') as f:
         config = yaml.load(f, yaml.FullLoader)
         print(json.dumps(config, indent=1), flush=True)
 
